965415e9a1e78efe7672c6b0a30c1752eaa3d633
Emmanuel Santana <hdsemmanuelsantana@users.noreply.github.com>
2025-05-30 17:13:13 -0400

    Update plugin-update-checker.php

--- daily-wp-rocket-cache-clear/plugin-update-checker/plugin-update-checker.php
+++ daily-wp-rocket-cache-clear/plugin-update-checker/plugin-update-checker.php
@@ -1,1 +1,13 @@
-// Plugin Update Checker main file placeholder
+<?php
+/**
+ * Plugin Update Checker Library (v4)
+ * 
+ * https://github.com/YahnisElsts/plugin-update-checker
+ */
+
+require_once dirname(__FILE__) . '/Puc/v4p10/Factory.php';
+
+if (function_exists('Puc_v4_Factory::addSource')) {
+    // Optional: You can register this PUC source globally for convenience.
+    Puc_v4_Factory::addSource(__FILE__);
+}
